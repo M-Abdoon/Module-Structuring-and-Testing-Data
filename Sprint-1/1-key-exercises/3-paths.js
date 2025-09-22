b82600e2aa411e0498791374b571f52160fa001d
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-09-22 19:37:43 +0100

    created 2 variables in 3-paths.js , one is to store the fir part of the filepath variable, the other is store the ext part of the variable

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,14 +10,19 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
+
 const lastSlashIndex = filePath.lastIndexOf("/");
+
 const base = filePath.slice(lastSlashIndex + 1);
+
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex + 1);
+const ext = filePath.slice(filePath.lastIndexOf("."));
 
+// to test >>
+//console.log(`The dir part of filePath is (( ${dir} )) AND the ext part of it is (( ${ext} ))`);
 // https://www.google.com/search?q=slice+mdn
