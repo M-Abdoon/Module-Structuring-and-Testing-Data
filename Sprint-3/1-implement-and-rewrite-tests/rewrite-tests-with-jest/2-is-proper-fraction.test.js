049c6289f0a25a5303cff52b13e15022e0d09802
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-09 03:27:33 +0100

    rewrite: is proper fraction exersice done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper Fractions", () => {
+  expect(isProperFraction(9, 4)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative Fractions", () => {
+  expect(isProperFraction(-6, 4)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for an equal Fractions", () => {
+  expect(isProperFraction(7, 7)).toEqual(false);
+});
