6cd7b357fcc116ae08ff09b2fcab94d2d3c91067
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-09 03:40:24 +0100

    rewrite: get card value test exercise done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,25 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return the number entered as input", () => {
+  const numberCards = getCardValue("4♠");
+  expect(numberCards).toEqual(4);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards J Q K", () => {
+  const faceCards = getCardValue("Q♠");
+  expect(faceCards).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 after entering Ace A", () => {
+  const handleAce = getCardValue("A♠");
+  expect(handleAce).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test(`should return string ("Invalid card rank.") for invalid inputs`, () => {
+  const invalidCards = getCardValue("HelloWorld!");
+  expect(invalidCards).toEqual("Invalid card rank.");
+});
