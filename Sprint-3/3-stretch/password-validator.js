5a9a70380d695a019e25a518e0482a4d27fad6fa
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-11 02:27:56 +0100

    made the password validator function, all validations finished and tested

--- Sprint-3/3-stretch/password-validator.js
+++ Sprint-3/3-stretch/password-validator.js
@@ -1,6 +1,70 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+    let index = 0;
+    let characters = "abcdefghijklmnopqrstuvwxyz";
+    let checker = false;
+    if (password.length < 5) {
+        return false;
+    }
+    while (index <= password.length) {
+        if (characters.toUpperCase().includes(password[index])) {
+            checker = true;
+        }
+        index++;
+    }
+    if(checker == false)
+        return false;
+
+    checker = false;
+    index = 0;
+    while (index <= password.length) {
+        if (characters.includes(password[index])) {
+            checker = true;
+        }
+        index++;
+    }
+    if(checker == false)
+        return false;
+
+    checker = false;
+    index = 0;
+    while (index <= password.length) {
+        if ("0123456789".includes(password[index])) {
+            checker = true;
+        }
+        index++;
+    }
+    if(checker == false)
+        return false;
+
+    checker = false;
+    index = 0;
+    while (index <= password.length) {
+        if ("!#$%.*&".includes(password[index])) {
+            checker = true;
+        }
+        index++;
+    }
+    if(checker == false)
+        return false;
+
+    index = 0;
+    while (index <= passwordsArray.length) {
+        if (passwordsArray[index] === password) {
+            return false;
+        }
+        index++;
+    }
+
+    return checker;
 }
 
+const passwordsArray = [];
+passwordsArray.push("123HowAreYou?");
+passwordsArray.push("ahmed@pass1");
+passwordsArray.push("123JohnIsHere#");
+passwordsArray.push("This_is_my_password?");
+passwordsArray.push("!!I_Am_Sam_Movie!!1");
 
-module.exports = passwordValidator;
+module.exports = passwordValidator; 
+
+//console.log(passwordValidator("setrwcrat!ahmddAed@pass1"));
--- Sprint-3/3-stretch/password-validator.test.js
+++ Sprint-3/3-stretch/password-validator.test.js
@@ -17,10 +17,50 @@ You must breakdown this problem in order to solve it. Find one test case first a
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
     // Arrange
-    const password = "12345";
+    const password = "1!Hel";
     // Act
     const result = isValidPassword(password);
     // Assert
     expect(result).toEqual(true);
+}
+);
+
+test("Have at least one English uppercase letter (A-Z)", () => {
+    // Arrange
+    const password = "1!HelloWorld_A";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+test("Have at least one English lowercase letter (a-z)", () => {
+    // Arrange
+    const password = "1!HelloWorld_a";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+test("Have at least one number (0-9)", () => {
+    // Arrange
+    const password = "1!HelloWorld_1";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+test(`Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")`, () => {
+    // Arrange
+    const password = "!!I_Am_Sam_Movie!!1";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(false);
 }
 );
