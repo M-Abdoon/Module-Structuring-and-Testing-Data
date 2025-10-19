c0d9240d1e01404ca4a6b9dd218558966a144603
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-19 12:07:18 +0100

    Updated the funciton so it handels other tests, and added more tests too

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,7 +1,28 @@
 function getOrdinalNumber(num) {
-  if(num === 1)
-    return "1st";
-  return 0;
+  //
+  if (typeof num !== "number" || !Number.isInteger(num) || num <= 0) {
+    return "Input must be a positive integer.";
+  }
+  
+  const lastTwoDigits = num % 100;
+  const lastDigit = num % 10;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return `${num}th`;
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
+  return num;  
 }
 
+console.log(getOrdinalNumber(21));    // "1st"
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -10,4 +10,54 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(101)).toEqual("101st");
+});
+
+// Case 2: Identify the ordinal number for 1
+// except those ending in 12
+test("append 'nd' to numbers ending in 2, except those ending in 12", () => {
+    expect( getOrdinalNumber(2) ).toEqual("2nd");
+    expect( getOrdinalNumber(22) ).toEqual("22nd");
+    expect( getOrdinalNumber(132) ).toEqual("132nd");
+});
+
+// Case 3: Identify the ordinal numbers for 3 to 12
+test("should return correct ordinal suffixes for numbers 3 to 12", () => {
+    expect( getOrdinalNumber(3) ).toEqual("3rd");
+    expect( getOrdinalNumber(4) ).toEqual("4th");
+    expect( getOrdinalNumber(5) ).toEqual("5th");
+    expect( getOrdinalNumber(6) ).toEqual("6th");
+    expect( getOrdinalNumber(7) ).toEqual("7th");
+    expect( getOrdinalNumber(8) ).toEqual("8th");
+    expect( getOrdinalNumber(9) ).toEqual("9th");
+    expect( getOrdinalNumber(10) ).toEqual("10th");
+    expect( getOrdinalNumber(11) ).toEqual("11th");
+    expect( getOrdinalNumber(12) ).toEqual("12th");
+});
+// Case 4: Identify the ordinal numbers for numbers above 12
+test("should return correct ordinal suffixes for numbers above 12", () => {
+    expect( getOrdinalNumber(13) ).toEqual("13th");
+    expect( getOrdinalNumber(19) ).toEqual("19th");
+    expect( getOrdinalNumber(23) ).toEqual("23rd");
+    expect( getOrdinalNumber(34) ).toEqual("34th");
+    expect( getOrdinalNumber(45) ).toEqual("45th");
+    expect( getOrdinalNumber(56) ).toEqual("56th");
+    expect( getOrdinalNumber(67) ).toEqual("67th");
+});
+
+// Case 5: Identify the ordinal numbers for numbers like 20 30 40 etc
+test("should return correct ordinal suffixes for multiples of ten", () => {
+    expect( getOrdinalNumber(20) ).toEqual("20th");
+    expect( getOrdinalNumber(30) ).toEqual("30th");
+    expect( getOrdinalNumber(70) ).toEqual("70th");
+    expect( getOrdinalNumber(50) ).toEqual("50th");
+});
+
+// Case 6: dealing with big numbers
+test("should return correct ordinal suffixes for big numbers", () => {
+    expect( getOrdinalNumber(111) ).toEqual("111th");
+    expect( getOrdinalNumber(4712) ).toEqual("4712th");
+    expect( getOrdinalNumber(10003) ).toEqual("10003rd");
+    expect( getOrdinalNumber(10012) ).toEqual("10012th");
 });
