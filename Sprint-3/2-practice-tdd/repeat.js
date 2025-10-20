4b42131f53aad28a37b5f749f05fc5aa81ddd897
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-20 23:50:33 +0100

    corrected condition rule to include NaN and Infinity numbers

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,5 @@
 function repeat(str, count) {
-  if (typeof count !== "number" || isNaN(count) || count < 0) {
+  if (!Number.isInteger(count) || count < 0) {
     throw new Error("Count must be a valid number");
   }
   return str.repeat(count);
