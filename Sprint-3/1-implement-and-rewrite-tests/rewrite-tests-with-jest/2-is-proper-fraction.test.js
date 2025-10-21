f8f268c541c5a47a2fe361b51f4b1b081acea959
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-21 01:12:53 +0100

    added more test cases withing each test()

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,19 +4,37 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
+  expect(isProperFraction(3, 2)).toEqual(false);
+  expect(isProperFraction(5, 8)).toEqual(true);
+  expect(isProperFraction(8, 5)).toEqual(false);
+  expect(isProperFraction(1, 4)).toEqual(true);
+  expect(isProperFraction(4, 1)).toEqual(false);
+  expect(isProperFraction(1, 4)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
 test("should return false for an improper Fractions", () => {
   expect(isProperFraction(9, 4)).toEqual(false);
+  expect(isProperFraction(4, 9)).toEqual(true);
+  expect(isProperFraction(7, 3)).toEqual(false);
+  expect(isProperFraction(3, 7)).toEqual(true);
+  expect(isProperFraction(10, 2)).toEqual(false);
+  expect(isProperFraction(2, 10)).toEqual(true);
 });
 
 // Case 3: Identify Negative Fractions:
-test("should return true for a negative Fractions", () => {
-  expect(isProperFraction(-6, 4)).toEqual(true);
+test("should return false for a negative Fractions", () => {
+  expect(isProperFraction(-6, 4)).toEqual(false);
+  expect(isProperFraction(-4, 6)).toEqual(true);
+  expect(isProperFraction(6, -4)).toEqual(false);
+  expect(isProperFraction(4, -6)).toEqual(true);   
+  expect(isProperFraction(-6, -4)).toEqual(false);
+  expect(isProperFraction(-4, -6)).toEqual(true);  
 });
 
 // Case 4: Identify Equal Numerator and Denominator:
 test("should return false for an equal Fractions", () => {
   expect(isProperFraction(7, 7)).toEqual(false);
+  expect(isProperFraction(10, 10)).toEqual(false);
+  expect(isProperFraction(1, 1)).toEqual(false);
 });
